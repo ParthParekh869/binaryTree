44f7959706aa0e641b0e6de98bc28da9fb071549
ParthParekh869 <parekhpr@myumanitoba.ca>
2024-06-21 19:13:02 -0500

    added Node.java file to the project

---
+++ Node.java
@@ -0,0 +1,26 @@
+public class Node {
+    private int data;
+    private  Node leftChild;
+    private Node rightChild;
+    public Node(int data){
+        this.data=data;
+    }
+    public int getData(){
+        return data;
+    }
+    public void setData(int data){
+        this.data=data;
+    }
+    public Node getLeftChild(){
+        return leftChild;
+    }
+    public void leftChild(Node leftChild){
+        this.leftChild=leftChild;
+    }
+    public Node getRightChild(){
+        return rightChild;
+    }
+    public void setRightChild(Node rightChild){
+        this.rightChild=rightChild;
+    }
+}
